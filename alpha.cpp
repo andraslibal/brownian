1de098d5d012a2b2b5c314d0e545a746604441d1
Bajcsi Adel <bajcsiadel@yahoo.com>
2018-02-02 09:44:25 +0200

    AB: make modofied

--- alpha.cpp
+++ alpha.cpp
@@ -14,8 +14,10 @@ double *y;
 // forces acting on a paricle
 double *fx;
 double *fy;
-// particle's color array
+// particles color array
 int *color;
+// particles electric charge array
+double *q;
 // system size
 double Sx, Sy;
 
@@ -36,23 +38,55 @@ int initParticles() {
 }
 
 int generateCoordinates() {
+    double Sx_2 = Sx / 2.0;
+    double Sy_2 = Sy / 2.0;
+
     for (int i = 0; i < N; i++) {
         ID[i] = i;
-        // choose a random position in the system for the particle
-        double tmpX = Sx * Rand();
-        double tmpY = Sy * Rand();
-        // checking if in this position there already is an element
-        for (int j = 0; j < i; j++) {
-            double diffX = x[j] - tmpX;
-            double diffY = y[j] - tmpY;
 
-            
-        }
+        int j;
+        double tmpX;
+        double tmpY;
+        do {
+            // choose a random position in the system for the particle
+            tmpX = Sx * Rand();
+            tmpY = Sy * Rand();
+            // checking if in this position there already is an element
+            for (j = 0; j < i; j++) {
+                double diffX = x[j] - tmpX;
+                double diffY = y[j] - tmpY;
+
+                if (diffX > Sx_2) diffX -= Sx;
+                if (diffX < -Sx_2) diffX += Sx;
+                if (diffX > Sy_2) diffX -= Sy;
+                if (diffX < -Sy_2) diffX += Sy;
+
+                double diffSquare = diffX * diffX + diffY * diffY;
+                if (diffSquare < 0.2 * 0.2) 
+                    break;
+            }
+        // if a managed to iterate over the whole bunch of particles which have their 
+        // position that means there wasn't any overlap so the new particle's coordinates
+        // are alright 
+        // ------
+        // ELSE the new element has overlap with one element from the system hence is 
+        // needed to regenerate them
+        } while (j != i);
+
+        x[i] = tmpX;
+        y[i] = tmpY;
+
+        if (Rand() > 0.5) color[i] = 2;
+        else color[i] = 1;
 
     }
 }
 
 int main() {
-    
+    int i = 1, j;
+    do {
+        for (j = 0; j < i; j++) cout << "FOR j: " << j << endl;
+        cout << "j: " << j << endl;
+    } while (j != i);
     return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,6 @@
 alpha:
 	g++ alpha.cpp -o alpha.out
+	./alpha.out
 
 clean:
 	rm alpha
